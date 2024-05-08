b7acd830552f0a25fac8f001763635751c7879aa
WilliamV <williamv1905@mhbx.org>
2024-05-08 19:32:57 -0400

    m

---
+++ src/main/java/SnakeGame.java
@@ -0,0 +1,16 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+      
+      new GameFrame();
+      new GamePanel();
+    }
+  
+    // @Test
+    // void addition() {
+    //     assertEquals(2, 1 + 1);
+    // }
+  }
